355c485ea5674cdcb2d92e40ddae36bd8dc969de
Flor Scaltritti <florlils729@gmail.com>
2025-11-26 20:02:29 -0300

    estiilosHd

---
+++ Front/header2.js
@@ -0,0 +1,23 @@
+/*HEADER PARA LAS OTRAS PANTALLAS*/
+document.addEventListener("DOMContentLoaded", () => {
+    const header = `
+        <header>
+            <a href="#" class="logo">
+                <img src="../img/logoPlats.svg" alt="PLATS logo">
+                PLATS
+            </a>
+            <nav>
+               <a href="./inicio.html">Inicio</a>
+               <a href="./biblioteca.html">Biblioteca compartida</a>
+               <a href="./nota.html">Notas</a>
+               <a href="">Foro</a>
+            </nav>
+            <div class="avatar-container">
+            <img id="FtAvatarUsuario" src="" alt="">
+        </div>
+        </header>
+    `;
+    
+    document.body.insertAdjacentHTML("afterbegin", header);
+});
+
